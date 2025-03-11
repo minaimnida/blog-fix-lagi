ee1537b050c79dd37eb66da33b5c7643a9762665
minaimnida <imaduddinmuhammad92@gmail.com>
2025-03-11 15:00:24 +0700

    gambar sudah ada

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lavishverse-us.backendless.app',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
